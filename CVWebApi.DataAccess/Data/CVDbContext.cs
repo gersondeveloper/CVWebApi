6fa12a1661805373c8f953d16abd63a064b45d0c
Gerson Cardoso Filho <gersoncfilho@mac.com>
2022-12-19 18:23:46 +0000

    removed comments from dbcontext

--- CVWebApi.DataAccess/Data/CVDbContext.cs
+++ CVWebApi.DataAccess/Data/CVDbContext.cs
@@ -24,18 +24,6 @@ public class CVDbContext : DbContext {
                     (c1, c2) => c1.SequenceEqual(c2),
                     c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
                     c => c.ToList()));
-
-        /*modelBuilder.Entity<Experience>(builder =>
-        {
-            builder.Property(x => x.TechnologiesList)
-                .HasConversion(new ValueConverter<IEnumerable<string>,string>(
-                    v => JsonConvert.SerializeObject(v),
-                    v => JsonConvert.DeserializeObject<List<string>>(v),
-                    (JsonSerializerOptions null,
-                        (c1, c2) => c1.SequenceEqual(c2),
-                    c => Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode()))),
-                    c => c.ToList()));
-        });*/
     }
 
 
